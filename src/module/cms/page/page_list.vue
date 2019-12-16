1d99c6264e87bfb78313b04cc356a3e6882d5182
wwt <1451963902@qq.com>
2019-12-16 17:53:29 +0800

    first commit

---
+++ src/module/cms/page/page_list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <!--编写页面静态部分，即view部分-->
+    测试页面显示...
+  </div>
+</template>
+
+<script>
+<!--编写页面静态部分，即model及vm部分-->
+</script>
+
+<style>
+<!--编写页面样式，不是必须-->
+</style>
---
+++ src/module/cms/router/index.js
@@ -0,0 +1,25 @@
+import Home from '@/module/cms/page/page_list.vue';
+export default [{
+    path: '/',
+    component: Home,
+    name: '系统管理首页',  //菜单名称
+    hidden: true,  //不显示true ,false 显示
+    children:[{
+      path: '/cms/page/list',
+      component: Home,
+      name: '页面列表',  //菜单名称
+      hidden: false,  //不显示true ,false 显示
+    }]
+    // ,
+    // redirect: '/home',
+    // children: [
+    //   {path: 'home', component: Home}
+    // ]
+  }/*,
+  {
+    path: '/login',
+    component: Login,
+    name: 'Login',
+    hidden: true
+  }*/
+]
